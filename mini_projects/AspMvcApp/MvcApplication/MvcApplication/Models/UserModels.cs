3c5390f8a5c551f05a9729e2fa1e57835f8df927
Musa.Zulu <zuluchs@gmail.com>
2015-01-07 10:48:40 +0200

    added new UserModels class

---
+++ mini_projects/AspMvcApp/MvcApplication/MvcApplication/Models/UserModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication.Models
+{
+    public class UserModels
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [Range(100,10000000)]
+        public decimal Salary { get; set; }
+    }
+}
